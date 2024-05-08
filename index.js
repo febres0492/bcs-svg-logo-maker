b03768bdf0bcf6a06fc20cb45c099d8b3fab84ab
Carlos Febres <febres0492@gmail.com>
2024-05-08 17:20:04 -0400

    |  adding inquirer questions to index.js |  adding setColor Func to shpe class |  fixing shape name |  creating utils.js

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 import inquirer from 'inquirer';
-import { Circle, Triangle, Rectangle } from './lib/shapes.js';
+import { Circle, Triangle, Square } from './lib/shapes.js';
+import { c } from './lib/utils.js'
 
-console.log(new Circle(5).render());
+//defining questions
+const questions = [
+    {name: 'text', type:'input', message: `What are the Initials. ${c('3 Max','y')}:`,
+        validate: input => { // Check if the input length is greater than 3
+            if (input.length > 3) { return 'Input must be 1 to 3 characters.' }
+            return true;
+        }
+    },
+    {name: 'text_color' , type: 'input', message: `What is the ${c('Text Color')}? ${c('hexadecimal number or color name','y')}:\n`},
+    {name: 'shape'      , type: 'list',  message: `What is the ${c('Shape')}?` , choices:['Circle','Square','Triangle']},
+    {name: 'shape_color', type: 'input', message: `What is the ${c('Shape Color')}? ${c('hexadecimal number or color name', 'y')}:\n`},
+]
+
+inquirer.prompt(questions).then((res) => {
+    console.log(res)
+    console.log('create file logo.svg 300x200')
+    console.log('step: create file logo.svg')
+    console.log(`Generated logo.svg`)
+})
+
