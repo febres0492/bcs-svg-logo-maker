8419f63ae32b078b7cb4344ad892d43673d7b77e
Carlos Febres <febres0492@gmail.com>
2024-05-08 20:10:02 -0400

    Update: - updating inquirer questions. - adding logic for color validation.

--- index.js
+++ index.js
@@ -2,17 +2,31 @@ import inquirer from 'inquirer';
 import { Circle, Triangle, Square } from './lib/shapes.js';
 import { c } from './lib/utils.js'
 
+const colorList = [
+    "black", "white", "red", "blue", "green", "yellow", "orange", "purple", "pink", 
+    "brown", "gray", "navy", "teal", "aqua", "lime", "maroon", "olive", "silver", "gold", 
+    "fuchsia", "cyan", "magenta", "beige", "coral", "ivory", "khaki", "lavender", "mint", 
+    "orchid", "salmon", "sienna", "tan", "violet", "azure", "chartreuse", "indigo", "plum", 
+    "tomato", "turquoise", "chocolate", "crimson", "darkblue", "darkcyan", "darkgrey", 
+    "darkred", "darksalmon", "darkseagreen", "firebrick", "forestgreen", "gainsboro"
+]
+
 //defining questions
 const questions = [
-    {name: 'text', type:'input', message: `What are the Initials. ${c('3 Max','y')}:`,
+    { name: 'text', type:'input', message: `What are the ${c('Initials')} for the Logo. ${c('(3 characters max)','y')}:\n`,
         validate: input => { // Check if the input length is greater than 3
-            if (input.length > 3) { return 'Input must be 1 to 3 characters.' }
+            input = input.trim()
+            if (input.length > 3 || input.length == 0) { return c('Input must be 1 to 3 characters.','y') }
             return true;
         }
     },
-    {name: 'text_color' , type: 'input', message: `What is the ${c('Text Color')}? ${c('hexadecimal number or color name','y')}:\n`},
-    {name: 'shape'      , type: 'list',  message: `What is the ${c('Shape')}?` , choices:['Circle','Square','Triangle']},
-    {name: 'shape_color', type: 'input', message: `What is the ${c('Shape Color')}? ${c('hexadecimal number or color name', 'y')}:\n`},
+    { name: 'text_color' , type: 'input', message: `What is the ${c('Text Color')}? ${c('( #Hexadecimal Number or Color name )','y')}:\n`,
+        validate: input => validateColor(input)
+    },
+    { name: 'shape'      , type: 'list',  message: `What is the ${c('Shape')}?` , choices:['Circle','Square','Triangle']},
+    { name: 'shape_color', type: 'input', message: `What is the ${c('Shape Color')}? ${c('( #Hexadecimal Number or Color name )', 'y')}:\n`,
+        validate: input => validateColor(input)
+    },
 ]
 
 inquirer.prompt(questions).then((res) => {
@@ -21,4 +35,11 @@ inquirer.prompt(questions).then((res) => {
     console.log('step: create file logo.svg')
     console.log(`Generated logo.svg`)
 })
+
+function validateColor(input){
+    if (/^#([0-9a-fA-F]{3}([0-9a-fA-F]{3})?)$/.test(input)) { return true }
+    if(colorList.indexOf(input) > -1 ) { return true }
+    if(input[0] == "#"){return c("Hex color doesnt Macht propper pattern. Input #------",'y')}
+    return c('Color name not recognized, Try a different color or a Hex color starting with "#".','y')
+}
 
